539936c566bfd6b218d5dba1cdf7d452b821f0af
sughra malik <sugraaaaa@outlook.com>
2015-04-24 21:33:39 +0530

    added a repo

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+//
+
+#include<iostream>
+#include<math.h>
+
+//we will add more headers as we need them
+
+using namespace std;
+
+
+
+int main(void)
+{
+	//
+	system("pause"); //for stopping the console on runtime [like getch()]
+	return 0;
+}
